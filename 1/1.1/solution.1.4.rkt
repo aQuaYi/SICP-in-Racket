2f1b3664378a3c70601b8b234f8fc247b6fb6ec5
aQua <aQuaYi@users.noreply.github.com>
2017-06-08 15:53:58 +0800

    add solution.1.4

--- 1/1.1/solution.1.4.rkt
+++ 1/1.1/solution.1.4.rkt
@@ -2,10 +2,13 @@
 
 ;当b>0时，a和b做+运算
 ;否则，a和b做-运算
+;注意: 在racket中，+-是函数，不是运算符。
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
+(display "(a-plus-abs-b 2 3) = ")
 (a-plus-abs-b 2 3)
 
+(display "(a-plus-abs-b 2 -4) = ")
 (a-plus-abs-b 2 -4)
