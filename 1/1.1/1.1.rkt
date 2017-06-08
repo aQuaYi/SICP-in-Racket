4877b6b81c4a56c36bcb3fd9becb775fdcf990fd
aQua <aQuaYi@users.noreply.github.com>
2017-06-08 15:17:07 +0800

    添加了1.1的答案

--- 1/1.1/1.1.rkt
+++ 1/1.1/1.1.rkt
@@ -18,3 +18,30 @@
 ;表达式的更抽象
 (define y (* x x))
 y
+;定义过程,是一种更强大的抽象过程。
+;用(square x)来代表(* x x)的过程。x可以是任何数据。
+(define (square x) (* x x))
+(square 12)
+
+(define (sum-of-squares x y)
+  (+ (square x)(square y)))
+(sum-of-squares 3 4)
+
+(define (f a)
+  (sum-of-squares (+ a 1)(* a 2)))
+(f 5)
+
+(define (abs x)
+  (cond ((> x 0)x)
+        ((= x 0)0)
+        ((< x 0)(- x))))
+(abs -7)
+
+(define (abs-with-if x)
+  (if (< x 0)
+      (- x)
+      x))
+(abs-with-if -8)
+
+(define z 7)
+(and (> z 5)(< z 10))
