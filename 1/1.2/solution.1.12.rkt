0388834f40a80811d178075bd2d9cf963565d724
aQua <aQuaYi@users.noreply.github.com>
2017-06-15 11:24:07 +0800

    细微修改了solution.1.12

--- 1/1.2/solution.1.12.rkt
+++ 1/1.2/solution.1.12.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
 (define (pascal row col)
-  (cond ((or (< row 0) (< col 0) (< row col)) 0)
-        ((= row col) 1)
+  (cond ((or (< col 0) (< row col)) 0)
+        ((or (= row col) (= col 0)) 1)
         (else (+ (pascal (- row 1) (- col 1))
                  (pascal (- row 1) col)))))
 
 (pascal 4 2)
-
-(pascal 1024 256)
+(pascal 8 4)
+(pascal 4 10)
