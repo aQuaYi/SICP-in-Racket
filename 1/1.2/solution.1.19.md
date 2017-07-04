0ca2a10b8bf150fd8e637577b8537f478bdc383c
aQua <aQua@aQuaYi.com>
2017-07-04 10:22:29 +0800

    添加习题解答.1.19

---
+++ 1/1.2/solution.1.19.md
@@ -0,0 +1,24 @@
+# 习题解答 1.19
+
+三次变换中的数，分别用a,b,a',b',a'',b''来表示
+
+可知
+
+a' = bq+aq+ap
+
+b' = bp+aq
+
+a'' = b'q+a'q+a'p
+
+b'' = b'p+a'q
+
+其中
+
+b'' = b'p+a'q=(bp+aq)p + (bq+aq+ap)q = b(pp+qq) + a(2pq+qq)
+
+所以
+
+p' = pp+qq
+q' = 2pq+qq
+
+可以把p'和q'带入a''进行验算，可知其正确性。
---
+++ 1/1.2/solution.1.19.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (p-next p q)
+  (+ (* p p)
+     (* q q)))
+
+(define (q-next p q)
+  (+ (* 2 p q)
+     (* q q)))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (p-next p q)
+                   (q-next p q)
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(fib 12)
