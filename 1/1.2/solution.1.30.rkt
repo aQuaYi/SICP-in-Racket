825c9b3363d09b2d2fed4a4a5a5df4581458770f
aQua <aQua@aQuaYi.com>
2017-07-05 14:10:07 +0800

    add solution.1.30

---
+++ 1/1.2/solution.1.30.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube n)
+  (* n n n))
+
+(define (inc n)
+  (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (parameter k)
+    (+ a
+       (* k h)))
+  (define (si-term k)
+    (cond ((or (= k 0)  (= k n)) (f (parameter k)))
+          ((= 1 (remainder k 2)) (* 4 (f (parameter k))))
+          (else (* 2 (f (parameter k))))))
+  (* (sum si-term 0 inc n)
+     (/ h 3.0)))
+
+(integral cube 0 1 0.01)
+(simpson-integral cube 0.0 1.0 100)
+
+(integral cube 0 1 0.001)
+(simpson-integral cube 0.0 1.0 1000)
+
+(integral cube 0 1 0.0001)
+
+(integral cube 0 1 0.00001)
