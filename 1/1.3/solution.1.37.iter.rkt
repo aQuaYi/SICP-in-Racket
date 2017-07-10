3b9b67c96d61d9d799a6ed358ca49bde4098d29a
aQua <aQua@aQuaYi.com>
2017-07-10 17:10:46 +0800

    add solution.1.37

--- 1/1.3/solution.1.37.iter.rkt
+++ 1/1.3/solution.1.37.iter.rkt
@@ -4,12 +4,11 @@
     (cont-frac-iter n d k 0))
 
 (define (cont-frac-iter n d i result)
-    (define ())
+    (define (next result)
+      (/ (n i) (+ (d i) result)))
     (if (= i 0)
         result
-        (cont-frac-iter n d (- i 1) )
-        (/ (n i) (d i))
-        (/ (n i) (+ (d i) (cont-frac-cur n d k (+ i 1))))))
+        (cont-frac-iter n d (- i 1) (next result))))
 
 (cont-frac  (lambda (i) 1.0)
             (lambda (i) 1.0)
