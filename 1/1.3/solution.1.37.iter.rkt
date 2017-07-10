1b2ab924af424ca23e63463d581fae1b61173e84
aQua <aQua@aQuaYi.com>
2017-07-10 17:33:58 +0800

    重构了 solution.1.37

--- 1/1.3/solution.1.37.iter.rkt
+++ 1/1.3/solution.1.37.iter.rkt
@@ -1,14 +1,15 @@
 #lang racket
 
-(define (cont-frac n d k) 
-    (cont-frac-iter n d k 0))
-
-(define (cont-frac-iter n d i result)
-    (define (next result)
-      (/ (n i) (+ (d i) result)))
+(define (cont-frac n d k)
+  (define (iter i result)
     (if (= i 0)
         result
-        (cont-frac-iter n d (- i 1) (next result))))
+        (iter (- i 1)
+              (/ (n i)
+                 (+ (d i) result)))))
+  (iter (- k 1)
+        (/ (n k)
+           (d k))))
 
 (cont-frac  (lambda (i) 1.0)
             (lambda (i) 1.0)
