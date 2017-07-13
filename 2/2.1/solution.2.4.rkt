ad726a576926c9c11f0ccb98988897da569b5fb5
aQua <aQua@aQuaYi.com>
2017-07-13 15:06:53 +0800

    add solution.2.4

---
+++ 2/2.1/solution.2.4.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(define z (cons 0 1))
+
+(car z)
+(cdr z)
