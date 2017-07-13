0d76d923201c3820fdc8c546f7f54a8db96a92a3
aQua <aQua@aQuaYi.com>
2017-07-13 17:30:09 +0800

    fix solution.2.6

--- 2/2.1/solution.2.6.md
+++ 2/2.1/solution.2.6.md
@@ -16,10 +16,17 @@
 (define three
   (lambda (f) (lambda (x) (f (f (f x))))))
 
-(define (plus a b) 
-  (lambda (x) (a (b x))))
+(define (plus m n)
+  (lambda (f)
+    (lambda (x)
+      ((m f) ((n f) x)))))
+
+(define (mult m n)
+  (lambda (f)
+    (lambda (x)
+      ((m (n f)) x))))
 ```
 
 `zero`, `one`, `two`和`three`都是高阶过程，他们的参数`f`是某个需要一个参数的一般过程。他们的区别在于嵌套作用于`x`的次数。
 
-那么，`(plus a b)`中的plus，就是把`a`和`b`这两个高阶过程，嵌套在一起。
+那么，`(plus m n)`中的plus，就是把`m`和`n`这两个高阶过程，嵌套在一起。
--- 2/2.1/solution.2.6.rkt
+++ 2/2.1/solution.2.6.rkt
@@ -14,25 +14,35 @@
 (define three
   (lambda (f) (lambda (x) (f (f (f x))))))
 
-(define (plus a b) 
-  (lambda (x) (a (b x))))
-
 (define inc
   (lambda (x) (+ 1 x)))
 
-(define one+
-  (one inc))
+((two inc) 1)
 
-(define two+
-  (two inc))
+(define (plus m n)
+  (lambda (f)
+    (lambda (x)
+      ((m f) ((n f) x)))))
 
-(define three+
-  (plus one+ two+))
+(define (mult m n)
+  (lambda (f)
+    (lambda (x)
+      ((m (n f)) x))))
 
-(three+ 0)
+(define five
+  (plus two three))
+
+(define five-inc
+  (five inc))
+
+(five-inc 3)
 
 (define double
   (lambda (x) (* 2 x)))
 
-((three double) 3)
+((three double) 1)
 
+(((plus two three) double) 1)
+
+;;; 2^15=32768
+(((mult three five) double) 1)
