835d13f1b6855bac05745a66444a6268945c10d1
aQua <aQua@aQuaYi.com>
2017-07-13 15:26:37 +0800

    add solution.2.5

---
+++ 2/2.1/solution.2.5.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(define (log x base)
+  (if (= 0 (remainder x base))
+      (+ 1 ( log (/ x base) base))
+      0))
+
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car z)
+  (log z 2))
+
+(define (cdr z)
+  (log z 3))
+
+(define z (cons 3 7))
+
+(displayln z)
+
+(car z)
+
+(cdr z)
