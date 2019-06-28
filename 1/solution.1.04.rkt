f54e482a8f4d8595606135f65021ecd2d900c0bb
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-28 15:42:38 +0800

    添加到了练习 1.5

--- 1/solution.1.04.rkt
+++ 1/solution.1.04.rkt
@@ -1,8 +1,8 @@
-#lang racket
+#lang sicp
 
-;当b>0时，a和b做+运算
-;否则，a和b做-运算
-;注意: 在racket中，+-是函数，不是运算符。
+;当 b>0 时，a 和 b 做 + 运算
+;否则，a 和 b 做 - 运算
+;注意: 在 racket 中，+- 是函数，不是运算符。
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
