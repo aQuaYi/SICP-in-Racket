fe610fcbe90fbc43fb4ea37ab6faade946fd14b5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-09 10:16:01 +0800

    1.32 修复错误

--- 1/1.3/solution.1.33.rkt
+++ 1/1.3/solution.1.33.rkt
@@ -1,21 +1,15 @@
 #lang sicp
 
-(define (accumulate combiner null-value term a next b)
+(define (filtered-accumulate filter combiner null-value term a next b)
   (if (> a b)
       null-value
       (combiner (term a)
-                (accumulate combiner null-value term (next a) next b))))
+                (filtered-accumulate filter combiner null-value term (next a) next b))))
 
 (define (product term a next b)
-  (accumulate * 1 term a next b))
+  (filtered-accumulate * * 1 term a next b))
 
 ;; -----
-
-(define (factorial n)
-  (define (identity x) x)
-  (product identity 1 inc n))
-
-(factorial 5)
 
 (define (quarter-pi)
   (define (term k)
