216607620f7f5ff4b951922fcecc6ba6f5a51095
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-09 10:44:29 +0800

    1.33 done

--- 1/1.3/solution.1.33.rkt
+++ 1/1.3/solution.1.33.rkt
@@ -3,20 +3,51 @@
 (define (filtered-accumulate filter combiner null-value term a next b)
   (if (> a b)
       null-value
-      (combiner (term a)
+      (combiner (if (filter a)
+                    (term a)
+                    null-value)
                 (filtered-accumulate filter combiner null-value term (next a) next b))))
+;; a)
+;;
+;; prime?
 
-(define (product term a next b)
-  (filtered-accumulate * * 1 term a next b))
+(define (prime? n)
+  (= n (smallest-divisor n)))
 
-;; -----
+(define (smallest-divisor n)
+  (find-divisor n 2))
 
-(define (quarter-pi)
-  (define (term k)
-    (/ (* (- k 1) (+ k 1))
-       (* k k)))
-  (define (next k)
-    (+ k 2))
-  (product term 3.0 next 1000000))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
 
-(* 4 (quarter-pi))
+(define (divides? d n)
+  (= (remainder n d)
+     0))
+
+(define (square a)
+  (* a a))
+
+;; prime-sum
+
+(define (prime-sum a b)
+  (filtered-accumulate prime? + 0 identity a inc b))
+
+(prime-sum 2 10)
+
+;; b)
+
+(define (gcd n a)
+  (if (= a 0)
+      n
+      (gcd a (remainder n a))))
+
+(define (product-relatively-prime n)
+  (define (relatively-prime?? n)
+    (define (relatively-prime? a)
+      (= 1 (gcd n a)))
+    relatively-prime?)
+  (filtered-accumulate (relatively-prime?? n) * 1 identity 2 inc (- n 1)))
+
+(product-relatively-prime 10)
