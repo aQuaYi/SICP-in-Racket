08f713fbfbad733964bba05c62a88ac38fae25d3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-10 10:39:29 +0800

    1.37 done

---
+++ 1/1.3/solution.1.37.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+; a) recursion
+
+(define (cont-frac n d k)
+  (define (frac c)
+    (if (= c 0)
+        0
+        (/ (n c)
+           (+ (d c)
+              (frac (- c 1))))))
+  (frac k))
+
+(cont-frac (lambda (i) 1.)
+           (lambda (i) 1.)
+           100)
+
+; b) iteration
+
+(define (cont-frac-iter n d k)
+  (define (frac f c)
+    (if (= c 0)
+        f
+        (frac (/ (n c)
+                 (+ (d c) f))
+              (- c 1))))
+  (frac (/ (n k) (d k)) (- k 1)))
+
+(cont-frac-iter (lambda (i) 1.)
+                (lambda (i) 1.)
+                100)
