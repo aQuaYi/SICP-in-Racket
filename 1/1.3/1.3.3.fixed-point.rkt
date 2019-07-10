03c0dad6875775a82e3f42c15bc08e699c83e2c4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-10 09:00:44 +0800

    fixed-point added

--- 1/1.3/1.3.3.fixed-point.rkt
+++ 1/1.3/1.3.3.fixed-point.rkt
@@ -1,40 +1,26 @@
 #lang sicp
 
-(define (average a b)
-  (/ (+ a b)
-     2.0))
+(define  tolerance 0.000001)
 
-(define (search-with-tolerance f neg-point pos-point max-tolerance)
+(define (fixed-point f first-guess)
   (define (close-enough? a b)
-    (< (abs (- a b)) max-tolerance))
-  (define (search f n p)
-    (let ((m (average n p)))
-      (if (close-enough? n p)
-          m
-          (let ((m-value (f m)))
-            (cond ((positive? m-value) (search f n m))
-                  ((negative? m-value) (search f m p))
-                  (else m))))))
-  (search f neg-point pos-point))
+    (< (abs (- a b)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? next guess)
+          next
+          (try next))))
+  (try first-guess))
 
+(fixed-point cos 1.0)
 
-(define (half-interval-method f a b)
-  (let ((max-tolerance 0.001)
-        (a-value (f a))
-        (b-value (f b)))
-    (cond ((= a-value 0) a)
-          ((= b-value 0) b)
-          ((and (negative? a-value) (positive? b-value))
-           (search-with-tolerance f a b max-tolerance))
-          ((and (negative? b-value) (positive? a-value))
-           (search-with-tolerance f b a max-tolerance))
-          (else
-           (error "Values are not of opposite sign," a b)))))
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
 
-(half-interval-method sin 2. 4.)
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ (+ y
+                                 (/ x y))
+                              2.0))
+               1.))
 
-(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
-                      1.
-                      2.)
-
-(half-interval-method sin 5. 4.)
+(sqrt 2)
