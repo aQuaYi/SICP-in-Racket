63fa36e127841bdf8c94ebbbe1a821657c89e049
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-19 10:23:59 +0800

    2.36 done

--- 2/2.2/solution.2.36.rkt
+++ 2/2.2/solution.2.36.rkt
@@ -13,22 +13,10 @@
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 
-(define (cars seqs)
-  (if (null? seqs)
-      nil
-      (cons (car (car seqs))
-            (cars (cdr seqs)))))
-
-(define (cdrs seqs)
-  (if (null? seqs)
-      nil
-      (cons (cdr (car seqs))
-            (cdrs (cdr seqs)))))
-
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
-      (cons (accumulate op init (cars seqs))
-            (accumulate-n op init (cdrs seqs)))))
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
 
 (accumulate-n + 0 seqs)
