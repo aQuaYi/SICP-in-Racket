fc34c21b27d725e83921ae51a9d30e7371455708
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-29 11:39:51 +0800

    2.58.b done

---
+++ 2/2.3/2.3.2/solution.2.58.b.rkt
@@ -0,0 +1,65 @@
+#lang sicp
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type --DERIV" exp))))
+
+;;; definination
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (addend s) (car s))
+  
+(define (augend x) 
+  (cleaner (cddr x))) 
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand x) 
+  (cleaner (cddr x))) 
+
+(define (=number? exp num)
+  (and (number? exp) (number? num) (= exp num)))
+
+(define (cleaner sequence) 
+  (if (null? (cdr sequence)) 
+      (car sequence) 
+      sequence)) 
+
+(deriv '(x + 3 * (x + y + 2)) 'x)
+
+(deriv '((x * y) + (8 * x)) 'x)
