d61b1d4af8a6f920e67d2c6b0f19946ae859b16b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-30 14:11:56 +0800

    2.65 done

--- 2/2.3/2.3.3/solution.2.65.rkt
+++ 2/2.3/2.3.3/solution.2.65.rkt
@@ -2,4 +2,8 @@
 
 (#%require "binary-tree.rkt")
 
-(list->tree '(1 3 5 7 9 11))
+(union-set (list->tree '(1 3 5 7 9 11))
+           (list->tree '(2 4 6 8 10)))
+
+(intersection-set (list->tree '(1 3 5 6 7 8 9 10 11))
+                  (list->tree '(1 2 3 4 5 6 8 10)))
