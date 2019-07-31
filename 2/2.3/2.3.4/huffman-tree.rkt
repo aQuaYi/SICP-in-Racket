dec006aea3b98cb5c84a61d0377052f2f4183d82
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-31 11:14:11 +0800

    2.68 done

---
+++ 2/2.3/2.3.4/huffman-tree.rkt
@@ -0,0 +1,92 @@
+#lang sicp
+
+(#%require (only racket provide))
+
+(provide make-leaf)
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(provide make-code-tree)
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(provide decode)
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+;;; 2.68
+
+(provide encode)
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+  
+(define (encode-symbol symbol tree)
+  (define (find symbol tree)
+    (cond ((leaf? tree) '())
+          ((element-of-set? symbol (symbols (left-branch tree)))
+           (cons 0 (encode-symbol symbol (left-branch tree))))
+          (else
+           (cons 1 (encode-symbol symbol (right-branch tree))))))
+  (if (element-of-set? symbol (symbols tree))
+      (find symbol tree) 
+      (error "try to encode NO exist symbol -- ENCODE-SYMBOL" symbol)))       
---
+++ 2/2.3/2.3.4/solution.2.67.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(#%require "huffman-tree.rkt")
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree (make-leaf 'B 2)
+                                  (make-code-tree (make-leaf 'D 1)
+                                                  (make-leaf 'C 1)))))
+
+(define sample-message
+  '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+ ;; '(0 1 1 0 0 1 0 1 0 1 1 1 0) 
+ ;; '(A D     A B   B   C     A)
+
+(decode sample-message sample-tree)
