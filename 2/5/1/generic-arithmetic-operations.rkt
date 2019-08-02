cc8c7ec72680dd26acccaab26472b5905e96de13
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-02 16:52:38 +0800

    2.76 skip

---
+++ 2/5/1/generic-arithmetic-operations.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+
+(#%require (only racket provide))
+
+(define (add x y) (apply-generic 'add x y))
+
+(define (sub x y) (apply-generic 'sub x y))
+
+(define (mul x y) (apply-generic 'mul x y))
+
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
