93dd75fd002ca86098742d8e0c01e45ffa4aa2df
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-09 16:46:10 +0800

    3.17 done

---
+++ 3/3/1/solution.3.17.rkt
@@ -0,0 +1,64 @@
+#lang sicp
+
+(define (count-pairs x)
+  (let ([counted '()])
+    (define (seen? x)
+      (cond ((memq x counted) 0)
+            (else  
+             (set! counted (cons x counted)) 
+             1)))
+    (define (count x) 
+      (if (not (pair? x)) 
+          0 
+          (+ (count (car x)) 
+             (count (cdr x)) 
+             (seen? x)))) 
+    (count x))) 
+
+(define str1 '(foo bar baz)) 
+(count-pairs str1) ; 3 
+; str1 -> ( . ) -> ( . ) -> ( . ) ->null 
+;          |        |        | 
+;          v        v        v 
+;         'foo     'bar     'baz 
+  
+(define x '(foo)) 
+(define y (cons x x)) 
+(define str2 (list y)) 
+(count-pairs str2) ; 4 
+; str2 -> ( . ) -> null 
+;          | 
+;          v 
+;         ( . ) 
+;          | | 
+;          v v 
+;         ( . ) -> 'null 
+;          | 
+;          v 
+;         'foo 
+  
+; (define x '(foo)) 
+; (define y (cons x x)) 
+(define str3 (cons y y)) 
+(count-pairs str3) ; 7 
+; str3 -> ( . ) 
+;          | | 
+;          v v 
+;         ( . ) 
+;          | | 
+;          v v 
+;         ( . ) -> null 
+;          | 
+;          v 
+;         'foo 
+  
+(define str4 '(foo bar baz)) 
+(set-cdr! (cddr str4) str4) 
+(count-pairs str4) ; maximum recursion depth exceeded 
+;          ,-------------------, 
+;          |                   | 
+;          v                   | 
+; str4 -> ( . ) -> ( . ) -> ( . ) 
+;          |        |        | 
+;          v        v        v 
+;         'foo     'bar     'baz 
