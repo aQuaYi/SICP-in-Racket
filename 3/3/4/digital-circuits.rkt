9008f529a9c4f83dc522f6ef2e7cb7342d98681d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-16 11:55:52 +0800

    3.3.4 adding wire

---
+++ 3/3/4/digital-circuits.rkt
@@ -0,0 +1,148 @@
+#lang sicp
+
+;;; wire
+
+(define (call-each procedures)
+  (cond ((null? procedures) 'done)
+        (else
+         ((car procedures))
+         (call-each (cdr procedures)))))
+
+(define (make-wire)
+  (let ([signal-value 0]
+        [action-procedures '()])
+    (define (set-my-signal! new-value)
+      (cond ((= signal-value new-value) 'done)
+            (else
+             (set! signal-value new-value)
+             (call-each action-procedures))))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc)) ;;; 加入时，立刻运行一次
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknow operation -- WIRE" m))))
+    dispatch))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+;;; delay
+
+(define (after-delay delay action)
+  (add-to-agenda! 
+   (+ delay (current-time the-agenda))
+   action
+   the-agenda))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time 
+           (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! 
+         (segment-queue (car segments))
+         action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment 
+                      time 
+                      action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment 
+                time 
+                action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) 
+  (car (segments agenda)))
+(define (rest-segments agenda) 
+  (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+;;; inverter
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ([new-value (logical-not (get-signal input))])
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+;;; and-gate
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ([new-value
+           (logical-and (get-signal a1)
+                        (get-signal a2))])
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1)
+              (= s2 1))
+         1)
+        (else 0)))
+
+;;; or-gate
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ([new-value
+           (logical-or (get-signal a1)
+                       (get-signal a2))])
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-or s1 s2)
+  (cond ((or (= s1 1)
+             (= s2 1))
+         1)
+        (else 0)))
