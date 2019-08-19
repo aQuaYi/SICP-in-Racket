7ef73a497d8ab8d35734503a11358dc578d8b29d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-19 13:08:00 +0800

    3.3.4 doing

--- 3/3/4/digital-circuits.rkt
+++ 3/3/4/digital-circuits.rkt
@@ -1,40 +1,5 @@
 #lang sicp
 
-;;; wire
-
-(define (call-each procedures)
-  (cond ((null? procedures) 'done)
-        (else
-         ((car procedures))
-         (call-each (cdr procedures)))))
-
-(define (make-wire)
-  (let ([signal-value 0]
-        [action-procedures '()])
-    (define (set-my-signal! new-value)
-      (cond ((= signal-value new-value) 'done)
-            (else
-             (set! signal-value new-value)
-             (call-each action-procedures))))
-    (define (accept-action-procedure! proc)
-      (set! action-procedures (cons proc action-procedures))
-      (proc)) ;;; 加入时，立刻运行一次
-    (define (dispatch m)
-      (cond ((eq? m 'get-signal) signal-value)
-            ((eq? m 'set-signal!) set-my-signal!)
-            ((eq? m 'add-action!) accept-action-procedure!)
-            (else (error "Unknow operation -- WIRE" m))))
-    dispatch))
-
-(define (get-signal wire)
-  (wire 'get-signal))
-
-(define (set-signal! wire new-value)
-  ((wire 'set-signal!) new-value))
-
-(define (add-action! wire action-procedure)
-  ((wire 'add-action!) action-procedure))
-
 ;;; delay
 
 (define (after-delay delay action)
@@ -91,6 +56,82 @@
 (define (empty-agenda? agenda)
   (null? (segments agenda)))
 
+
+;;;
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-queue) (cons '() '()))
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) 
+  (set-car! queue item))
+(define (set-rear-ptr! queue item) 
+  (set-cdr! queue item))
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) 
+                          new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with 
+                 an empty queue" queue))
+        (else (set-front-ptr! 
+               queue 
+               (cdr (front-ptr queue)))
+              queue)))
+(define (empty-queue? queue) 
+  (null? (front-ptr queue)))
+
+;;; wire
+
+(define (call-each procedures)
+  (cond ((null? procedures) 'done)
+        (else
+         ((car procedures))
+         (call-each (cdr procedures)))))
+
+(define (make-wire)
+  (let ([signal-value 0]
+        [action-procedures '()])
+    (define (set-my-signal! new-value)
+      (cond ((= signal-value new-value) 'done)
+            (else
+             (set! signal-value new-value)
+             (call-each action-procedures))))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc)) ;;; 加入时,立刻运行一次
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknow operation -- WIRE" m))))
+    dispatch))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
 ;;; inverter
 
 (define (inverter input output)
@@ -137,8 +178,8 @@
       (after-delay or-gate-delay
                    (lambda ()
                      (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
   'ok)
 
 (define (logical-or s1 s2)
@@ -146,3 +187,5 @@
              (= s2 1))
          1)
         (else 0)))
+
+;;; 
