dd6c1b793592d68e222e1d3ff43d8711f44b83f8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-09 10:28:11 +0800

    3.51 added

---
+++ 3/5/1/solution.3.51.rkt
@@ -0,0 +1,39 @@
+#lang sicp
+
+(define  (show x)
+  (display-line x)
+  x)
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+;;;
+
+(define (stream-car s) (car s))
+(define (stream-cdr s) (force (cdr s)))
+
+(define (stream-map proc s)
+  (if (stream-null?)
+      the-empty-stream
+      (cons-stream
+       (proc (stream-car s))
+       (stream-map proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+
+(stream-ref x 5)
+
+(stream-ref x 7)
